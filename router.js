f9db53f2e5dcf7e1e3243a0b42a51a900932eef7
shiftypig <simon.cunningham@synchrotron.org.au>
2013-12-11 09:34:37 +1100

    add banner image

--- router.js
+++ router.js
@@ -6,6 +6,7 @@ module.exports = router
 var public = require('./routes/public')
 router.addRoute('/css/*?', public)
 router.addRoute('/js/*?', public)
+router.addRoute('/images/*?', public)
 
 router.addRoute('/', require('./routes/index'))
 router.addRoute('/login', require('./routes/login'))
