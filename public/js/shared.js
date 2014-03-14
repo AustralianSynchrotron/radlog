d6b2d7196b588431ea4e4b22f5b363dd816f0e1c
Simon Cunningham <simon.cunningham@synchrotron.org.au>
2014-03-14 18:43:16 +1100

    Add confirmation before deleting sources.

--- public/js/shared.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.delete-button').click(function(e) {
-    $.ajax({
-        url: $(this).attr('action')
-      , type: 'DELETE'
-      , success: function() { location.reload() }
-    })
-  })
-})
